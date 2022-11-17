cfdaa830e71e068d1b7002a380cd5637594f3d75
imran-samad <imransamad7@gmail.com>
2022-11-17 09:27:35 +0100

    Added comments about using git and functions

--- Norway_Github.R
+++ Norway_Github.R
@@ -6,14 +6,14 @@ use_git_config(
   user.email = "imransamad7@gmail.com"
 ) # do this only once
 
-usethis::create_github_token() # use this when your token expires. If you save the token in this script, it will be invalidated by git
-gitcreds::gitcreds_set()
-#cb4a382e6b09fca4959f392d59194d8485dfaa11
-#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
-git_vaccinate()
+usethis::create_github_token() # use this when your token expires. If you save the token in this script, it will be invalidated by git after pushing
+gitcreds::gitcreds_set()# use this when you have generated a new token. A token provides temporary access (30 days) to your account
 
-usethis::use_git()
+git_vaccinate()# use this only once. This command creates a list of extensions that are never committed
+
+usethis::use_git()# initialises a Git repository and adds important files to .gitignore. If user consents, it also makes an initial commit.
 
 use_github()
-git_default_branch_rename()
-git_default_branch_configure(name = "main")
+
+git_default_branch_rename() # use this only the first time to change the 'master' branch to 'main' branch
+git_default_branch_configure(name = "main") # this will ensure all following repos have the primary branch named as 'main'
